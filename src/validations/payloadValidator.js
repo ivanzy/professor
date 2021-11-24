d9d602f5bdd50cffb732f92f67e1a85e110d1df2
ivan <idimitryz@gmail.com>
2021-11-24 11:34:25 +0100

    validing new payload format

--- src/validations/payloadValidator.js
+++ src/validations/payloadValidator.js
@@ -7,8 +7,39 @@ const validate = (payload) => {
     return false;
   if (!(payload.time && typeof payload.time === "number"))
     return false;
-  if (!(payload.url && typeof payload.url === "string"))
+  if (!(payload.url && typeof payload.url === "object"))
     return false;
+  if (
+    !(
+      payload.url.get && typeof payload.url.get === "string"
+    )
+  )
+    return false;
+  if (
+    !(
+      payload.url.post && typeof payload.url.post === "string"
+    )
+  )
+    return false;
+  if (
+    !(
+      payload.url.put && typeof payload.url.put === "string"
+    )
+  )
+    return false;
+  if (
+    !(
+      payload.url.delete && typeof payload.url.delete === "string"
+    )
+  )
+    return false;
+  if (
+    !(
+      payload.url.patch && typeof payload.url.patch === "string"
+    )
+  )
+    return false;
+
   return true;
 };
 
