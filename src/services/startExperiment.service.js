412829c82da6b2b43c24372b3b083ed4564dae7b
ivan <idimitryz@gmail.com>
2023-09-20 12:40:02 +0200

    feat: create payload for acc and acoustic sensors with and without features

--- src/services/startExperiment.service.js
+++ src/services/startExperiment.service.js
@@ -1,7 +1,6 @@
 const logger = require("../config/logger");
 const performRequest = require("./performRequest");
 const gConfig = require("../config/conf.json");
-const chooseMethod = require("../utils/chooseMethod");
 const randomGenerator = require("../utils/randomGenerator");
 
 //* wait a exponential time and perform a call to the service
@@ -9,40 +8,36 @@ const start = async ({
   url,
   numberOfEdges,
   name,
-  wot = false,
+  withFeatures,
 }) =>
   await new Promise((resolve, reject) => {
-    const periodicity = gConfig.periodicity;
-    for (let i = 0; i < numberOfEdges; i++) {
-      //create three acc
-      for (
-        let i = 0;
-        i < gConfig.numberOfAccelerometersPerEdge;
-        i++
-      ) {
-        setTimeout(() => {
-          performRequest.accelerometer(url, name, wot);
-          resolve();
-        }, randomGenerator.getRandomInt(periodicity));
-      }
-
-      for (
-        let i = 0;
-        i < gConfig.numberOfAcousticEmissionSensorsPerEdge;
-        i++
-      ) {
-        setTimeout(() => {
-          performRequest.acoustic(url, name, wot);
-          resolve();
-        }, randomGenerator.getRandomInt(periodicity));
-      }
-    }
-
     logger.info(
-      `Going to perform a ${method} call in ${(
-        time / 1000
-      ).toFixed(2)}s`
+      `Going to create ${numberOfEdges} edge nodes`
     );
+    for (let i = 0; i < numberOfEdges; i++) {
+      createDevices(
+        "accelerometer",
+        url,
+        name,
+        withFeatures
+      );
+      createDevices(
+        "acousticEmission",
+        url,
+        name,
+        withFeatures
+      );
+    }
   });
+
+createDevices = (type, url, name, withFeatures) => {
+  const config = gConfig[type];
+  for (let i = 0; i < config.perEdgeNode; i++) {
+    setTimeout(() => {
+      performRequest(type, url, name, withFeatures);
+      resolve();
+    }, randomGenerator.getRandomInt(config.periodicity));
+  }
+};
 
 module.exports = start;
