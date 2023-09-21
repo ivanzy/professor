99986ac0c06fe27efb457700a529e56d1e87986d
ivan <idimitryz@gmail.com>
2023-09-21 18:35:37 +0200

    feat: wait 10s to finish the experiment

--- src/services/experimentTermination.js
+++ src/services/experimentTermination.js
@@ -2,13 +2,16 @@ const logger = require("../config/logger");
 const intervalManager = require("../utils/intervalManager");
 const record = require("../utils/recordDataPoint").recordExperimentManagement;
 const RequestCounterManager = require("../utils/RequestCounterManager");
-
+const experimentControl = require("../utils/experimentControl");
 const experimentTermination = ({ name, replication }) => {
-  logger.info(`terminating the experiment...`);
-  const ids = intervalManager.getIntervalIds();
-  for (const id of ids) clearInterval(id);
-  const recordEntry = `${name};${replication},${RequestCounterManager.getCounter()};${RequestCounterManager.getResponse()};${RequestCounterManager.pendingRequests()}`;
-  record(recordEntry);
+  logger.info(`terminating the experiment in 10s...`);
+  setTimeout(() => {
+    const ids = intervalManager.getIntervalIds();
+    for (const id of ids) clearInterval(id);
+    experimentControl.finish();
+    const recordEntry = `${name};${replication},${RequestCounterManager.getCounter()};${RequestCounterManager.getResponse()};${RequestCounterManager.pendingRequests()}`;
+    record(recordEntry);
+  }, 10000);
 };
 
 module.exports = experimentTermination;
